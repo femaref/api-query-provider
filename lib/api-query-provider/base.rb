241789d5c821f5fdaa5753a7f48d6c9eb78828dc
Femaref <femaref@googlemail.com>
2012-03-17 19:06:18 +0100

    invoking right method via send

--- lib/api-query-provider/base.rb
+++ lib/api-query-provider/base.rb
@@ -1,8 +1,12 @@
 module ApiQueryProvider
-  class Base < Provider      
+  class Base < Provider
     def initialize(data)
+      if self == ApiQueryProvider::Base
+        throw "this class should never be instanciated directly"
+      end
+    
       data.each do |key, value|
-        self.send(key.to_sym, value)
+        self.send("#{key}=".to_sym, value)
       end
     end
   end
