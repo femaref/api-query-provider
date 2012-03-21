5c79c9002fdc9be9d94819c04c7d3e9de984e770
Femaref <femaref@googlemail.com>
2012-03-22 00:17:32 +0100

    adjusted test for new api_url on TestClass

--- spec/attr_spec.rb
+++ spec/attr_spec.rb
@@ -4,7 +4,7 @@ require 'lib/github_commits'
 
 describe "ApiQueryProvider::Provider" do
   it "should not replace api_url and api_path of two classes" do
-    TestClass.api_url.should == "example.com"
+    TestClass.api_url.should == "http://example.com"
     TestClass.api_path.should == "/foo/:id/id"
     
     GithubCommits.api_url.should == "http://github.com/api/v2/json/"
